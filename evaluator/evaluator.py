6ff3acbec34cb2dfd68a750d2ecd756649a99554
Jonas Holtkamp <jonas.holtkamp@senacor.com>
2017-11-08 16:25:06 +0100

    Add evaluator.py

---
+++ evaluator/evaluator.py
@@ -0,0 +1,29 @@
+import json
+from pprint import pprint
+from TraderAI.trading.trader_interface import Portfolio, SharesOfCompany, StockMarketData
+
+rt = RandomTrader()
+
+
+# tradingAction = rt.doTrade(portfolio, stockMarketData)
+
+def read_portfolio() -> Portfolio:
+    json_data = open("json/portfolio.json").read()
+    data = json.loads(json_data)
+
+    shares_list = list()
+    for share in data['shares']:
+        shares_list.append(SharesOfCompany(share['name'], share['amount']))
+
+    pprint(shares_list[0].companyName)
+    pprint(shares_list[0].amountOfShares)
+    pprint(data['cash'])
+    return Portfolio(data['cash'], shares_list)
+
+
+def read_stock_market_data() -> StockMarketData:
+    return StockMarketData()
+
+
+read_portfolio()
+read_stock_market_data()
