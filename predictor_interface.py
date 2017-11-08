a839c08524682fc96e7725055d6d1a26ddeee72b
Janusz Tymoszuk <janusz.tymoszuk@senacor.com>
2017-11-08 16:14:11 +0100

    Predictor interface added.

---
+++ predictor_interface.py
@@ -0,0 +1,23 @@
+'''
+Created on 08.11.2017
+
+@author: jtymoszuk
+'''
+import abc
+
+class IPredictor(metaclass=abc.ABCMeta):
+    '''
+    Predictor interface
+    '''
+
+    @abc.abstractmethod
+    def doPredict(self, data:list) -> float:
+        """ Predicts future stock value of company using it's given historical values 
+    
+        Args:
+          data : historical stock values of a company 
+        Returns:
+          next predicted value of the company
+        """
+        pass
+        
