d2c0400e560aeaf2222f624c4074457d93e0d75b
Müller, Richard <bitbucket@richardmueller.net>
2017-11-16 17:27:22 +0100

    added testsuite for PerfectPredictor

--- test_suite_all.py
+++ test_suite_all.py
@@ -1,13 +1,14 @@
 import unittest
 
 from evaluating.test.evaluator_test import EvaluatorTest
+from predicting.test.perfect_predictor_test import PerfectPredictorTest
 from predicting.test.predictor_test import PredictingTest
 from trading.test.trader_test import TraderTest
 from predicting.test.perfect_stock_a_predictor_test import PerfectStockAPredictorTest
 
 if __name__ == "__main__":
 
-    test_classes = [EvaluatorTest, PredictingTest, PerfectStockAPredictorTest, TraderTest]
+    test_classes = [EvaluatorTest, PredictingTest, PerfectPredictorTest, PerfectStockAPredictorTest, TraderTest]
 
     suites = []
     for test in test_classes:
