86246a26a6fa2c8c0a969642a54522108c6413c7
Müller, Richard <bitbucket@richardmueller.net>
2017-11-22 21:42:09 +0100

    switched logging level to “info” by default

--- logger.py
+++ logger.py
@@ -6,5 +6,5 @@ Created on 16.11.2017
 import logging
 message_format="%(asctime)s, %(levelname)s, %(module)s:%(lineno)d: %(message)s"
 # Logging hierarchy (greater means more output): debug > info > warning > error
-logging.basicConfig(level=logging.DEBUG, format=message_format)
+logging.basicConfig(level=logging.INFO, format=message_format)
 logger = logging.getLogger(__name__)
