08a9d88f9224a32650c71289b0a1ba129856818f
Janusz Tymoszuk <janusz.tymoszuk@senacor.com>
2017-11-23 11:13:12 +0100

    Code camp preparation: predictor made ready (team blue only)

--- predicting/predictor/team_blue/team_blue_predictor.py
+++ predicting/predictor/team_blue/team_blue_predictor.py
@@ -4,7 +4,6 @@ Created on 08.11.2017
 @author: rmueller
 '''
 from model.IPredictor import IPredictor
-import numpy as np
 
 from model.StockData import StockData
 from utils import load_keras_sequential, save_keras_sequential, read_stock_market_data
@@ -13,25 +12,27 @@ from logger import logger
 from matplotlib import pyplot as plt
 from keras.models import Sequential
 from definitions import PERIOD_1, PERIOD_2
+from keras.callbacks import History
 
 TEAM_NAME = "team_blue"
 
-RELATIVE_PATH = 'predicting/predictor/' + TEAM_NAME + '/' + TEAM_NAME + '_predictor'
+RELATIVE_PATH = 'predicting/predictor/' + TEAM_NAME + '/' + TEAM_NAME + '_predictor_data'
 MODEL_FILE_NAME_STOCK_A = TEAM_NAME + '_predictor_stock_a_network'
 MODEL_FILE_NAME_STOCK_B = TEAM_NAME + '_predictor_stock_b_network'
 
-# Neural network configuration
+# Neural network configuration -> TODO see Keras Documentation
+INPUT_SIZE = 42  # TODO 
+FIRST_LAYER_SIZE = 42  # TODO
+SECOND_LAYER_SIZE = 42  # TODO
+OUTPUT_SIZE = 1  # TODO
+INNER_ACTIVATION_FUNCTION = 'relu'  # TODO
+ACTIVATION_FUNCTION_FOR_OUTPUT = 'linear'  # TDOO
+LOSS_FUNCTION = 'mean_squared_error'  # TODO
+OPTIMIZER = 'adam'  # TODO
+METRICS = ['accuracy']  # TODO
 
-#TODO: choose size
-INPUT_SIZE = 42
-FIRST_LAYER_SIZE = 42 
-SECOND_LAYER_SIZE = 42 
-OUTPUT_SIZE = 42
-ACTIVATION_FUNCTION_FOR_OUTPUT = 'TODO'
-LOSS_FUNCTION = 'TODO'
-OPTIMIZER = 'TODO'
-METRICS = ['TODO']
 BATCH_SIZE = 128
+
 
 class TeamBlueBasePredictor(IPredictor):
     '''
@@ -43,15 +44,10 @@ class TeamBlueBasePredictor(IPredictor):
         Constructor: Load the trained and stored neural network.
         '''
         # Try loading a stored trained neural network...
-        self.trained = True
         self.model = load_keras_sequential(RELATIVE_PATH, nn_filename)
-        # ... if that wasn't possible, then create a new untrained one
-        if self.model is None:
-            logger.warn(f"Loading of trained neural network failed, creating a new untrained one.")
-            self.trained = False
-            self.model = create_model()
-            
-        self.model.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER)
+        assert self.model is not None
+
+        # TODO compile loaded model
         
     def doPredict(self, data: StockData) -> float:
         """ Use the loaded trained neural network to predict the next stock value.
@@ -61,15 +57,9 @@ class TeamBlueBasePredictor(IPredictor):
         Returns:
           predicted next stock value for that company
         """
-        # Extract last 100 floats (here: stock values) as input for neural network (format: numpy array of arrays)
-        input_values = np.array([[x[1] for x in data.get_from_offset(-INPUT_SIZE)]])
-
-        try:
-            # Let network predict the next stock value based on last INPUT_SIZE stock values
-            return self.model.predict(input_values)
-        except:
-            logger.error("Error in predicting next stock value.")
-            assert False
+        
+        # TODO: extract needed data for neural network and predict result
+        return 0.0
 
 
 class TeamBlueStockAPredictor(TeamBlueBasePredictor):
@@ -101,55 +91,11 @@ class TeamBlueStockBPredictor(TeamBlueBasePredictor):
 
 
 def learn_nn_and_save(training_data: StockData, test_data: StockData, filename_to_save:str):
-    
-        
-    training_dates = training_data.get_dates()
-    training_prices = training_data.get_values()
-    test_prices = test_data.get_values()
-    
-    
-    # Build chunks of prices from INPUT_SIZE consecutive days (lastPrices) and 101th day (currentPrice)
-    lastPrices, currentPrice = [], []
-    for i in range(0, len(training_prices) - INPUT_SIZE):
-        lastPrices.append(training_prices[i:INPUT_SIZE + i])
-        currentPrice.append(float(training_prices[INPUT_SIZE + i]))
-
-    #the same for test data:
-    last_prices_test, current_price_test = [], []
-    for i in range(0, len(last_prices_test) - INPUT_SIZE):
-        last_prices_test.append(test_prices[i:INPUT_SIZE + i])
-        current_price_test.append(float(test_prices[INPUT_SIZE + i]))
-
+           
     network = create_model()
     
-    network.compile(loss=LOSS_FUNCTION, optimizer=OPTIMIZER)
-
-    # Train the neural network
-    history = network.fit(lastPrices, currentPrice, epochs=500, batch_size=BATCH_SIZE, verbose=1, validation_data=(last_prices_test, current_price_test), shuffle=True)
-
-    # Evaluate the trained neural network and plot results
-    score = network.evaluate(np.array(lastPrices), currentPrice, batch_size=BATCH_SIZE, verbose=0)
-    logger.debug(f"Test score: {score}")
-                     
-    #Draw results
-    plt.figure()
-    plt.plot(history.history['loss'])
-    plt.title('training loss / testing loss by epoch')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['training', 'testing'], loc='best')
+    # TODO: learn network and draw results
     
-    plt.figure()
-    currentPrice_prediction = network.predict(lastPrices, batch_size=BATCH_SIZE)
-    plt.plot(training_dates[INPUT_SIZE:], currentPrice, color="black")  # current prices in reality
-    plt.plot(training_dates[INPUT_SIZE:], currentPrice_prediction, color="green")  # predicted prices by neural network
-    plt.title('current prices / predicted prices by date')
-    plt.ylabel('price')
-    plt.xlabel('date')
-    plt.legend(['current', 'predicted'], loc='best')
-    
-    plt.show()
-
     # Save trained model: separate network structure (stored as JSON) and trained weights (stored as HDF5)
     save_keras_sequential(network, RELATIVE_PATH, filename_to_save)
 
@@ -157,26 +103,47 @@ def learn_nn_and_save(training_data: StockData, test_data: StockData, filename_t
 def create_model() -> Sequential:
     network = Sequential()
     
-    #TODO: add layers
-   
+    # TODO: build model
+
     return network
+
+
+def draw_history(history: History):
+    plt.figure()
+    plt.plot(history.history['loss'])
+    plt.title('training loss / testing loss by epoch')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['training', 'testing'], loc='best')
+
+  
+def draw_prediction(dates: list, awaited_results: list, prediction_results: list):  
+    plt.figure()
+
+    plt.plot(dates[INPUT_SIZE:], awaited_results, color="black")  # current prices in reality
+    plt.plot(dates[INPUT_SIZE:], prediction_results, color="green")  # predicted prices by neural network
+    plt.title('current prices / predicted prices by date')
+    plt.ylabel('price')
+    plt.xlabel('date')
+    plt.legend(['current', 'predicted'], loc='best')
+    
+    plt.show()
+
 
 if __name__ == "__main__":
     logger.debug("Data loading...")
     training_stock_market_data = read_stock_market_data([CompanyEnum.COMPANY_A, CompanyEnum.COMPANY_B], [PERIOD_1])
     test_stock_market_data = read_stock_market_data([CompanyEnum.COMPANY_A, CompanyEnum.COMPANY_B], [PERIOD_2])
     
-    company_a_StockData = training_stock_market_data.get_for_company(CompanyEnum.COMPANY_A)
-    datesA = company_a_StockData.get_dates()
-    pricesA = company_a_StockData.get_values()
+    company_a_training_stock_data: StockData = training_stock_market_data.get_for_company(CompanyEnum.COMPANY_A)
+    company_a_test_stock_data: StockData = test_stock_market_data.get_for_company(CompanyEnum.COMPANY_A)
     
-    logger.debug(f"Data for Stock A loaded: {len(pricesA)} prices and {len(datesA)} dates read.")
-    learn_nn_and_save(datesA, pricesA, MODEL_FILE_NAME_STOCK_A)
+    logger.debug(f"Data for Stock A loaded")
+    learn_nn_and_save(company_a_training_stock_data, company_a_test_stock_data, MODEL_FILE_NAME_STOCK_A)
     
-    company_b_StockData = training_stock_market_data.get_for_company(CompanyEnum.COMPANY_B)
-    datesB = company_b_StockData.get_dates()
-    pricesB = company_b_StockData.get_values()
+    company_b_training_stock_data: StockData = training_stock_market_data.get_for_company(CompanyEnum.COMPANY_B)
+    company_b_test_stock_data: StockData = test_stock_market_data.get_for_company(CompanyEnum.COMPANY_B)
     
-    logger.debug(f"Data for Stock B loaded: {len(pricesB)} prices and {len(datesB)} dates read.")
-    learn_nn_and_save(datesB, pricesB, MODEL_FILE_NAME_STOCK_B)
+    logger.debug(f"Data for Stock B loaded")
+    learn_nn_and_save(company_b_training_stock_data, company_b_test_stock_data, MODEL_FILE_NAME_STOCK_B)
 
