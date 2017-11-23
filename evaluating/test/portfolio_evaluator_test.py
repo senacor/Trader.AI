de1dbf79408ad0179ec0c7a051a4144790c88941
Jonas <jonasholtkamp@users.noreply.github.com>
2017-11-23 17:00:13 +0100

    Simplify stock exchange (#10)
    * Simplify stock exchange by creating a convenience method to create portfolio/trader mappings

--- evaluating/test/portfolio_evaluator_test.py
+++ evaluating/test/portfolio_evaluator_test.py
@@ -11,6 +11,7 @@ from datetime import date, datetime
 
 from definitions import PERIOD_1, PERIOD_2, PERIOD_3
 from model.StockData import StockData
+from predicting.predictor.reference.perfect_predictor import PerfectPredictor
 from utils import read_stock_market_data
 from evaluating.portfolio_evaluator import PortfolioEvaluator
 from model.CompanyEnum import CompanyEnum
@@ -22,11 +23,49 @@ from trading.trader.reference.simple_trader import SimpleTrader
 
 
 class EvaluatorTest(unittest.TestCase):
+    def test_different_mappings(self):
+        """
+        Tests: Evaluator#inspect_over_time and #inspect_over_time_with_mappings
+
+        Compares if both methods return the same values
+        """
+        stock_market_data = read_stock_market_data([CompanyEnum.COMPANY_A], [PERIOD_1, PERIOD_2, PERIOD_3])
+
+        portfolio_1 = Portfolio(20000, [SharesOfCompany(CompanyEnum.COMPANY_A, 200)])
+        portfolio_2 = Portfolio(20000, [SharesOfCompany(CompanyEnum.COMPANY_A, 200)])
+        portfolios = [portfolio_1, portfolio_2]
+
+        perfect_predictor = PerfectPredictor(CompanyEnum.COMPANY_A)
+
+        trader_1 = SimpleTrader(perfect_predictor, perfect_predictor)
+        trader_2 = SimpleTrader(perfect_predictor, perfect_predictor)
+        trader_3 = SimpleTrader(RandomPredictor(), RandomPredictor())
+        traders = [trader_1, trader_2]
+
+        portfolio_trader_mappings = list(zip(portfolios, traders))
+
+        evaluator_1 = PortfolioEvaluator([trader_1, trader_1])
+        evaluator_2 = PortfolioEvaluator([])
+        evaluator_3 = PortfolioEvaluator([trader_3, trader_3])
+
+        # 1 Use evaluator that is initialized *with* traders and call `#inspect_over_time` (i.e. without traders)
+        result_1 = evaluator_1.inspect_over_time(stock_market_data, portfolios, 100)
+
+        # 2 Use evaluator that is initialized *without* traders and call `#inspect_over_time_with_mapping`
+        # (i.e. with traders)
+        result_2 = evaluator_2.inspect_over_time_with_mapping(stock_market_data, portfolio_trader_mappings, 100)
+
+        # 3 Use evaluator that is initialized *with* traders and call `#inspect_over_time_with_mapping`
+        # (i.e. with traders) - this should be no problem, because the traders given at initialization time are ignored
+        result_3 = evaluator_3.inspect_over_time_with_mapping(stock_market_data, portfolio_trader_mappings, 100)
+
+        assert result_1 == result_2 == result_3
+
     def test_update_and_draw(self):
         """
         Tests: Evaluator#inspect_over_time
 
-        Creates a portfolio, a stock market data object
+        Inspects portfolios over time and checks the results
         """
         trader = SimpleTrader(RandomPredictor(), RandomPredictor())
 
@@ -52,6 +91,11 @@ class EvaluatorTest(unittest.TestCase):
         assert data_row_lengths.pop() == 100
 
     def test_inspect__default_offset(self):
+        """
+        Tests: Evaluator#inspect_over_time
+
+        Flavour: Test with an int offset
+        """
         data = StockData([(date(2017, 1, 1), 150.0), (date(2017, 1, 2), 200.0), (date(2017, 1, 3), 250.0)])
         stock_market_data = StockMarketData({CompanyEnum.COMPANY_A: data})
 
@@ -68,6 +112,11 @@ class EvaluatorTest(unittest.TestCase):
         assert date(2017, 1, 3) not in portfolio_over_time.keys()
 
     def test_inspect__date_offset(self):
+        """
+        Tests: Evaluator#inspect_over_time
+
+        Flavour: Test with an date offset
+        """
         data = StockData([(date(2017, 1, 1), 150.0), (date(2017, 1, 2), 200.0), (date(2017, 1, 3), 250.0)])
         stock_market_data = StockMarketData({CompanyEnum.COMPANY_A: data})
 
